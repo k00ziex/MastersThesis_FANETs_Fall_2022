370aa38b6a773b526928ec93ed22d84da9e859d9
Simon Jensen <simonzhjensen@gmail.com>
2022-09-07 09:31:17 +0200

    fixed port

--- TCP/client.py
+++ TCP/client.py
@@ -2,8 +2,8 @@
 
 import socket
 
-HOST = "127.0.0.1"  # The server's hostname or IP address
-PORT = 5050  # The port used by the server
+HOST = "10.1.0.100"  # The server's hostname or IP address
+PORT = 5005  # The port used by the server
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.connect((HOST, PORT))
