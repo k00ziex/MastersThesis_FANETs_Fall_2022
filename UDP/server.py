b4e44fe88587cf2e37764cc862027dfe21a29208
AndreasV <201705136@post.au.dk>
2022-10-19 13:15:32 +0200

    Removing latency

--- UDP/server.py
+++ UDP/server.py
@@ -2,7 +2,7 @@ from socket import socket, gethostbyname, AF_INET, SOCK_DGRAM
 PORT_NUMBER = 5005
 SIZE = 1024
 
-hostName = gethostbyname( '10.1.0.25' )
+hostName = gethostbyname( '10.1.0.100' )
 
 mySocket = socket( AF_INET, SOCK_DGRAM )
 mySocket.bind( (hostName, PORT_NUMBER) )
