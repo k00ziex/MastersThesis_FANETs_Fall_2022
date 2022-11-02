3e281f9c7d3c16bb7eca7e63a203578a71220fd9
AndreasV <201705136@post.au.dk>
2022-11-02 09:15:31 +0100

    iperf fixes

--- iperf.sh
+++ iperf.sh
@@ -7,7 +7,6 @@ interval=0
 while [ $interval -lt $2 ]
 do
     throughput_output="$(iperf3 -c $1 -t 10 --get-server-output --logfile /root/logfiles/iperf_results.txt)"
-    echo "SEE iperf_results.txt FOR OUTPUT" >> /root/logfiles/results.txt
     interval=$(($interval+1))
     sleep 10
 done
